d5ef0c57c77682bc40cadf62000d2bbe8f1d37ff
anujknmiet <anujmca_knmiet@yahoo.co.in>
2024-07-27 19:01:32 +0530

    Producer APi

---
+++ api/ProducerAPI/FITAppProducerAPI/FITAppProducerAPI/Repository/IProducerRepository.cs
@@ -0,0 +1,9 @@
+﻿using FITAppProducerAPI.Model;
+
+namespace FITAppProducerAPI.Repository
+{
+    public interface IProducerRepository
+    {
+        public CustomerReward GeRewards(int customerId, int athleteId, int fitnessPoints, int clientId);
+    }
+}
---
+++ api/ProducerAPI/FITAppProducerAPI/FITAppProducerAPI/Repository/ProducerRepository.cs
@@ -0,0 +1,18 @@
+﻿using FITAppProducerAPI.Model;
+
+namespace FITAppProducerAPI.Repository
+{
+    public class ProducerRepository:IProducerRepository
+    {
+        public CustomerReward GeRewards(int customerId, int athleteId, int fitnessPoints, int clientId)
+        {
+            double NWRewardRate = 0.10;
+            double rewards = fitnessPoints * NWRewardRate;
+            CustomerReward customerReward = new CustomerReward();
+            customerReward.athleteId = athleteId;
+            customerReward.customerId = customerId;
+            customerReward.rewardPoints = rewards;
+            return customerReward;
+        }
+    }
+}
